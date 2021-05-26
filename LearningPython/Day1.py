c8ac166ac0cf44dfd43b24a9f708b63483a5a16b
ayush <ayushsingh45556@gmail.com>
2021-05-26 09:49:23 +0530

    base change representation of a positive number

--- LearningPython/Day1.py
+++ LearningPython/Day1.py
@@ -1,1 +1,16 @@
-print("Hello Ayush")
+
+#function to represent a number in different base
+def from_bas_10(n, b):
+    if b < 2:
+        ValueError('base must be >= 2')
+    if n < 0:
+        ValueError('Number n must be positive')
+    if n == 0:
+        return [0]
+    digits = []
+    while n > 0:
+        n, m = n // b, n % b
+        digits.insert(0, m)
+    return digits
+
+print(from_bas_10(10, 2))
