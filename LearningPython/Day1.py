d0734d1a2b98ec68bc6dfe07d8734ea2c370f1a1
ayush <ayushsingh45556@gmail.com>
2021-05-26 10:00:34 +0530

    added base change function with encoding

--- LearningPython/Day1.py
+++ LearningPython/Day1.py
@@ -1,5 +1,4 @@
-
-#function to represent a number in different base
+# Function to represent a number in different base
 def from_bas_10(n, b):
     if b < 2:
         ValueError('base must be >= 2')
@@ -12,3 +11,27 @@ def from_bas_10(n, b):
         n, m = n // b, n % b
         digits.insert(0, m)
     return digits
+
+
+# Encoding function
+def encode(digits, digit_map):
+    if max(digits) >= len(digit_map):
+        raise ValueError('Digit map is not long enough to encode digits')
+    return ''.join(digit_map[d] for d in digits)
+
+
+# Base conversion function
+def rebase_from10(number, base):
+    digit_map = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    if base < 2 or base > 36:
+        ValueError('Invalid Base: 2 <= base <= 36')
+    sign = -1 if number < 0 else 1
+    number *= sign
+    digits = from_bas_10(number, base)
+    encoding = encode(digits, digit_map)
+    if sign == -1:
+        encoding = '-' + encoding
+    return encoding
+
+
+print(rebase_from10(10, 2))
